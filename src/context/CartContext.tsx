83ffd9850821d852d08eac39c6526f6c74b291e0
chihab <chihab.mg.me@gmail.com>
2024-12-15 00:02:46 +0100

    completed the menu context

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,21 +1,44 @@
 import { CartItem } from "@/interfaces/cartItem";
 import { createContext, ReactNode, useState } from "react";
 
-interface MenuType  {
-    items:CartItem[]
-
+interface MenuType {
+  items: CartItem[];
+  addItem: (item: CartItem) => void;
+  removeItem: (id: string) => void;
+  updateQuantity: (id: string, qt: number) => void;
 }
-const initialState:MenuType = {
-    items:[]
-}
+const initialState: MenuType = {
+  items: [],
+  addItem: () => {},
+  removeItem: () => {},
+  updateQuantity: () => {},
+};
 
-export const menuContext = createContext(initialState)
+export const menuContext = createContext(initialState);
 
-export const MenuProvider =  ({children}:{children:ReactNode})=>{
-    const [items,setItems] = useState(initialState.items)
-    const value:typeof initialState = {
-        items
-    }
-    return <menuContext.Provider value={value}>{children}</menuContext.Provider>
+export const MenuProvider = ({ children }: { children: ReactNode }) => {
+  const [items, setItems] = useState(initialState.items);
 
-}
+  const addItem = (item: CartItem) => {
+    setItems((prev) => [...prev, item]);
+  };
+
+  const removeItem = (id: string) => {
+    setItems((prev) => prev.filter((i) => i.id != id));
+  };
+  const updateQuantity = (id:string, qt:number) => {
+    setItems((prev) =>
+      prev.map((i) => {
+        if (i.id != id) return i;
+        return { ...i, quantity: qt };
+      })
+    );
+  };
+  const value: typeof initialState = {
+    items,
+    addItem,
+    removeItem,
+    updateQuantity
+  };
+  return <menuContext.Provider value={value}>{children}</menuContext.Provider>;
+};
